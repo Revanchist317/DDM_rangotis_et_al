1f8e62169b26e6582a75d51f0508154d919d3d9e
Revan <116514311+Revanchist317@users.noreply.github.com>
2023-12-10 10:46:00 +0100

    Update combine_subjects.m

--- Behavioural_psychophysics_and_preprocessing/combine_subjects.m
+++ Behavioural_psychophysics_and_preprocessing/combine_subjects.m
@@ -1,5 +1,5 @@
 
-cd('/Users/revanhome/Desktop/HDDM_test_MATLAB/Behavioural_psychophysics_and_preprocessing/Behavioural_data')
+cd('/Users/revanhome/Desktop/Magdeburg/DDM/Behavioural_psychophysics_and_preprocessing/Behavioural_data')
 
 subjects_in_directory = dir("*raw.csv");
 
@@ -10,6 +10,5 @@ for i = 1:length(subjects_in_directory)
         combined_blocks_humans = [combined_blocks_humans; readtable(subjects_in_directory(i).name)];  %#ok<AGROW>
     end
 end 
-
 % Save the new table of combined data 
 writetable(combined_blocks_humans, 'combined_blocks_humans_raw.csv')
