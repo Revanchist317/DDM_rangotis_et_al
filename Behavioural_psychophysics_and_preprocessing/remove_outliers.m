cb5e3f129e862bae6aae618b3807b40a378161b2
Revan <116514311+Revanchist317@users.noreply.github.com>
2023-12-10 10:59:43 +0100

    Update remove_outliers.m

--- Behavioural_psychophysics_and_preprocessing/remove_outliers.m
+++ Behavioural_psychophysics_and_preprocessing/remove_outliers.m
@@ -1,5 +1,5 @@
-% This script serves to remove outliers from the raw data and plots the
-% output as a figure 
+% This script serves to remove outliers from the raw data
+
 comb_subj_raw = readtable("combined_blocks_humans_raw.csv"); 
 
 % Plot the RT histograms of each task
@@ -8,12 +8,12 @@ rt_cyl_sac = comb_subj_raw.rt(comb_subj_raw.stimulus_displayed == "cylinder" & c
 rt_dots_butt =comb_subj_raw.rt(comb_subj_raw.stimulus_displayed == "dots" & comb_subj_raw.response_mode == "buttons", :);
 rt_dots_sac = comb_subj_raw.rt(comb_subj_raw.stimulus_displayed == "dots" & comb_subj_raw.response_mode == "saccades", :);
 
-% Plotting section 
 f = figure;
 tiledlayout("flow")
 
 nexttile
 histogram(rt_cyl_sac)
+% xline(mean(rt_cyl_sac),"-", "mean")
 title("Cylinder/saccades")
 q2 = quantile(rt_cyl_sac, 0.95);
 xlabel("Reaction time [seconds]")
@@ -26,6 +26,7 @@ xlim([0 3])
 
 nexttile
 histogram(rt_dots_sac)
+% xline(mean(rt_dots_sac),"-", "mean")
 title("RDK/saccades")
 q4 = quantile(rt_dots_sac, 0.95);
 xlabel("Reaction time [seconds]")
@@ -38,6 +39,7 @@ xlim([0 3.5])
 
 nexttile
 histogram(rt_cyl_butt)
+% xline(mean(rt_cyl_butt),"-", "mean")
 title("Cylinder/hand")
 q1 = quantile(rt_cyl_butt, 0.95);
 xlabel("Reaction time [seconds]")
@@ -50,6 +52,7 @@ xlim([0 3])
 
 nexttile
 histogram(rt_dots_butt)
+% xline(mean(rt_dots_butt),"-", "mean")
 title("RDK/hand")
 q3 = quantile(rt_dots_butt, 0.95);
 xlabel("Reaction time [seconds]")
@@ -60,11 +63,9 @@ xl4.LabelHorizontalAlignment = 'left';
 set(gca, "FontSize", 17)
 xlim([0 3.5])
 
-% Resize 
 f.Position = [366,251,1271,776];
 sgtitle("Outlier removal, human data", 'FontWeight', 'bold', 'FontSize', 30);
 
-% Remova outliers
 comb_subj_raw(comb_subj_raw.rt < 0.250, :) = [];
 comb_subj_raw(comb_subj_raw.stimulus_displayed == "cylinder" & comb_subj_raw.response_mode == "buttons" & comb_subj_raw.rt > q1, :) = [];
 comb_subj_raw(comb_subj_raw.stimulus_displayed == "cylinder" & comb_subj_raw.response_mode == "saccades" & comb_subj_raw.rt > q2, :) = [];
